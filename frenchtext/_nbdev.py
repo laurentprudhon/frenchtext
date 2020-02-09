9b603c2b9148bdb27c3be8bd8603af29b578b998
laurentprudhon <laurent.prudhon@hotmail.fr>
2020-02-09 18:25:37 +0100

    Work in progress

--- frenchtext/_nbdev.py
+++ frenchtext/_nbdev.py
@@ -2,9 +2,22 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"Config": "00_core.ipynb",
+         "config": "00_core.ipynb",
+         "download_url": "00_core.ipynb",
+         "pd.options.display.max_rows": "01_datasets.ipynb",
+         "pd.options.display.max_columns": "01_datasets.ipynb",
+         "list_datasets": "01_datasets.ipynb",
+         "datasetsdf": "01_datasets.ipynb",
+         "read_download_info": "01_datasets.ipynb",
+         "download_all_datasets": "01_datasets.ipynb",
+         "download_dataset_file": "01_datasets.ipynb",
+         "downloaddf": "01_datasets.ipynb",
+         "read_dataset_file": "01_datasets.ipynb",
+         "read_urls_file": "01_datasets.ipynb"}
 
-modules = []
+modules = ["core.py",
+           "datasets.py"]
 
 doc_url = "https://laurentprudhon.github.io/frenchtext/"
 
